bd82ff622d17b74d89431a2479222c2811385f6e
Renato Cabral <carvalho.renato6@gmail.com>
2015-01-10 00:15:00 -0200

    implementacaoes na classe conexao

--- cadastrodelivros/Conexao.java
+++ cadastrodelivros/Conexao.java
@@ -2,4 +2,26 @@ package cadastrodelivros;
 
 public class Conexao {
     
+    
+    //funções de controle
+    public void open(){
+    
+        
+    }
+    
+    public void close(){
+    
+    
+    }
+    
+    public void execute(){
+    
+    
+    }
+    
+    //variaveis de armazenamento
+    private final String url="jdbc:mysql://localhost:3306/Livro?zeroDateTimeBehavior=convertToNull[root em Esquema default]";
+    private final String user="root";
+    private final String password="123";
+    
 }
