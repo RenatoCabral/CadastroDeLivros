1071bc7a8df7401f2bacf8f46bded9a5d586589d
Renato Cabral <carvalho.renato6@gmail.com>
2015-01-12 21:54:39 -0200

    add comentario funcao cadastrar

--- cadastrodelivros/Cadastra.java
+++ cadastrodelivros/Cadastra.java
@@ -154,6 +154,7 @@ public class Cadastra extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
     
+    //botao cadastrar
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
